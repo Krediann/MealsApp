6405131b876ccbdcbceb73525c90a6a8ad94e5aa
Roope Lehtinen <Roope98.rl@gmail.com>
2023-10-25 12:40:26 +0300

    Changed titles from the categories

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -3,15 +3,15 @@ import Meal from '../models/meal';
 
 export const CATEGORIES = [
   new Category('c1', 'Italian', '#f5428d'),
-  new Category('c2', 'Quick & Easy', '#f54242'),
+  new Category('c2', 'Homefood', '#f54242'),
   new Category('c3', 'Hamburgers', '#f5a442'),
-  new Category('c4', 'German', '#f5d142'),
-  new Category('c5', 'Light & Lovely', '#368dff'),
+  new Category('c4', 'Finnish', '#f5d142'),
+  new Category('c5', 'Heavy & Lovely', '#368dff'),
   new Category('c6', 'Exotic', '#41d95d'),
-  new Category('c7', 'Breakfast', '#9eecff'),
+  new Category('c7', '2nd Breakfast', '#9eecff'),
   new Category('c8', 'Asian', '#b9ffb0'),
   new Category('c9', 'French', '#ffc7ff'),
-  new Category('c10', 'Summer', '#47fced')
+  new Category('c10', 'winter', '#47fced')
 ];
 
 export const MEALS = [
