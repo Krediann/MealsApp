3d68ba2aab5b64d1b624dddf2722d294b511e600
Roope Lehtinen <Roope98.rl@gmail.com>
2023-10-25 12:36:39 +0300

    Added native stack navigator

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
-import CategoryScreen from './screens/CategoryScreen';
+import CategoryScreen from "./screens/CategoryScreen";
+
+import { StatusBar } from "expo-status-bar"
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const screenStack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <CategoryScreen />
+    <>
+      <NavigationContainer>
+        <screenStack.Navigator>
+          <screenStack.Screen name = "Categories" component = {CategoryScreen} />
+        </screenStack.Navigator>
+      </NavigationContainer>
+      <StatusBar style = "dark" />
+    </>
   );
 }
